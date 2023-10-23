90f589ecd75b8b12ad35546958c4b20cea16eb07
Andrew <139555539+lilnephew22@users.noreply.github.com>
2023-10-23 19:57:02 +0000

    Update

---
+++ js/constants.js
@@ -0,0 +1,11 @@
+// К-сть товарів при першому рендері
+export const COUNT_SHOW_CARDS_CLICK = 4;
+
+export const ERROR_SERVER = 'Помилка серверу, спробуйте пізніше!';
+export const NO_PRODUCTS_IN_THIS_CATEGORY = 'Товарів у цій категорії немає!';
+export const PRODUCT_INFORMATION_NOT_FOUND = 'Інформація про товар не знайдена!';
+export const NO_ITEMS_CART = 'В корзині немає товарів!';
+
+
+
+
